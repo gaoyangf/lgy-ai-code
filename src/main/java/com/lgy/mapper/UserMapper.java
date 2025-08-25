dbbc468c42dc459e360a070b3228e46129c667fd
jbnz <158281673@qq.com>
2025-08-25 22:26:18 +0800

    feat(user): 添加用户相关功能- 新增用户注册、登录、注销等功能 - 实现用户信息的添加、查询、更新和删除 - 添加用户相关的数据传输对象和实体类 - 创建用户表和相关索引 -集成 MyBatis Flex 作为持久层框架 - 添加 AOP 切面实现权限控制

---
+++ src/main/java/com/lgy/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.lgy.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.lgy.model.entity.User;
+
+/**
+ * 用户 映射层。
+ *
+ * @author gy
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
